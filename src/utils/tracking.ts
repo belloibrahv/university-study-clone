ea1a9cb9dff9586932a781af0cd0efd69d6482c9
belloibrahv <belloibrahv@gmail.com>
2025-02-06 18:32:42 +0100

    refined version

--- src/utils/tracking.ts
+++ src/utils/tracking.ts
@@ -2,7 +2,7 @@ import { FilterState } from "../types";
 
 declare global {
   interface Window {
-    filterInteractions: FilterState & { results: any[] };
+    filterInteractions: Partial<FilterState> & { results: any[] };
   }
 }
 
@@ -17,8 +17,6 @@ const initializeFilterInteractions = () => {
       searchQuery: '',
       results: [],
       university: [],
-      coop: false,
-      remote: false,
     };
   }
 };
@@ -26,9 +24,34 @@ const initializeFilterInteractions = () => {
 export const updateFilterInteractions = (filters: FilterState & { results: any[] }) => {
   if (typeof window !== 'undefined') {
     initializeFilterInteractions();
+    
+    // Create a copy of filterInteractions to modify
+    const updatedInteractions = { ...window.filterInteractions };
+    
+    // Conditionally add coop field only if true
+    if (filters.coop) {
+      updatedInteractions.coop = filters.coop;
+    } else {
+      delete updatedInteractions.coop;
+    }
+    
+    // Conditionally add remote field only if true
+    if (filters.remote) {
+      updatedInteractions.remote = filters.remote;
+    } else {
+      delete updatedInteractions.remote;
+    }
+    
+    // Update other fields
     window.filterInteractions = {
-      ...window.filterInteractions,
-      ...filters
+      ...updatedInteractions,
+      programLevel: filters.programLevel,
+      language: filters.language,
+      studyArea: filters.studyArea,
+      province: filters.province,
+      searchQuery: filters.searchQuery,
+      results: filters.results,
+      university: filters.university
     };
   }
 };
